3e2c8f16f0d2ed28114d65854c96360dda396e5d
Hugo Baraúna <hugo.barauna@gmail.com>
2025-12-08 09:16:30 -0300

    Fix github source URL

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Typst.MixProject do
   use Mix.Project
 
-  @source_url "http://github.com/Hermanverschooten/typst"
+  @source_url "https://github.com/Hermanverschooten/typst"
   @version "0.2.0"
 
   @nerves_rust_target_triple_mapping %{
